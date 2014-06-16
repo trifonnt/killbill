trifonnt/killbill

commit 7aaca3914cc89bfdc1d652fe04095c4f6e7bfca5

More temp work for payment

api/src/main/java/org/killbill/billing/entity/EntityBase.java
@@ -90,10 +90,10 @@ public abstract class EntityBase implements Entity {
         if (id != null ? !id.equals(that.id) : that.id != null) {
             return false;
         }
-        if (createdDate != null ? !createdDate.equals(that.createdDate) : that.createdDate != null) {
+        if (createdDate != null ? createdDate.compareTo(that.createdDate) == 0 : that.createdDate != null) {
             return false;
         }
-        if (updatedDate != null ? !updatedDate.equals(that.updatedDate) : that.updatedDate != null) {
+        if (updatedDate != null ? updatedDate.compareTo(that.updatedDate) == 0 : that.updatedDate != null) {
             return false;
         }
 

api/src/main/java/org/killbill/billing/payment/api/PaymentInternalApi.java
@@ -18,21 +18,28 @@
 
 package org.killbill.billing.payment.api;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.Nullable;
+
 import org.killbill.billing.account.api.Account;
+import org.killbill.billing.callcontext.InternalCallContext;
 import org.killbill.billing.callcontext.InternalTenantContext;
 
 public interface PaymentInternalApi {
 
-    public Payment getPayment(UUID paymentId, Iterable<PluginProperty> properties, InternalTenantContext context)
+    public DirectPayment createPayment(final Account account, final UUID invoiceId,
+                                       @Nullable final BigDecimal amount, final Iterable<PluginProperty> properties, final InternalCallContext internalContext) throws PaymentApiException;
+
+    public DirectPayment getPayment(UUID paymentId, Iterable<PluginProperty> properties, InternalTenantContext context)
             throws PaymentApiException;
 
     public PaymentMethod getPaymentMethodById(UUID paymentMethodId, boolean includedInactive, Iterable<PluginProperty> properties, InternalTenantContext context)
             throws PaymentApiException;
 
-    public List<Payment> getAccountPayments(UUID accountId, InternalTenantContext context)
+    public List<DirectPayment> getAccountPayments(UUID accountId, InternalTenantContext context)
             throws PaymentApiException;
 
     public List<PaymentMethod> getPaymentMethods(Account account, Iterable<PluginProperty> properties, InternalTenantContext context)

api/src/main/java/org/killbill/billing/payment/api/PaymentService.java
@@ -22,6 +22,6 @@ public interface PaymentService extends KillbillService {
     @Override
     String getName();
 
-    PaymentApi getPaymentApi();
+    DirectPaymentApi getPaymentApi();
 
 }

payment/src/main/java/org/killbill/billing/payment/control/InvoicePaymentControlPluginApi.java
@@ -24,8 +24,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.joda.time.DateTime;
+import org.killbill.billing.account.api.Account;
 import org.killbill.billing.callcontext.InternalCallContext;
 import org.killbill.billing.callcontext.InternalTenantContext;
 import org.killbill.billing.invoice.api.Invoice;
@@ -35,9 +38,13 @@ import org.killbill.billing.invoice.api.InvoiceItem;
 import org.killbill.billing.payment.api.PaymentStatus;
 import org.killbill.billing.payment.api.PluginProperty;
 import org.killbill.billing.payment.api.TransactionType;
+import org.killbill.billing.payment.control.dao.InvoicePaymentControlDao;
+import org.killbill.billing.payment.control.dao.PluginAutoPayOffModelDao;
 import org.killbill.billing.payment.dao.DirectPaymentModelDao;
 import org.killbill.billing.payment.dao.DirectPaymentTransactionModelDao;
 import org.killbill.billing.payment.dao.PaymentDao;
+import org.killbill.billing.payment.glue.PaymentModule;
+import org.killbill.billing.payment.retry.BaseRetryService.RetryServiceScheduler;
 import org.killbill.billing.payment.retry.DefaultFailureCallResult;
 import org.killbill.billing.payment.retry.DefaultPriorPaymentControlResult;
 import org.killbill.billing.retry.plugin.api.PaymentControlApiException;
@@ -65,6 +72,7 @@ import com.google.common.collect.Iterables;
 public final class InvoicePaymentControlPluginApi implements PaymentControlPluginApi {
 
     public final static String PLUGIN_NAME = "__INVOICE_PAYMENT_CONTROL_PLUGIN__";
+    public final static String CREATED_BY = "InvoicePaymentControlPluginApi";
 
     private static final String IPCD_REFUND_IDS_WITH_AMOUNT_KEY = "IPCD_REF_IDS_AMOUNTS";
 
@@ -72,38 +80,26 @@ public final class InvoicePaymentControlPluginApi implements PaymentControlPlugi
     private final InvoiceInternalApi invoiceApi;
     private final TagUserApi tagApi;
     private final PaymentDao paymentDao;
+    private final InvoicePaymentControlDao controlDao;
+    private final RetryServiceScheduler retryServiceScheduler;
     private final InternalCallContextFactory internalCallContextFactory;
     private final Clock clock;
 
     private final Logger logger = LoggerFactory.getLogger(InvoicePaymentControlPluginApi.class);
 
+    @Inject
     public InvoicePaymentControlPluginApi(final PaymentConfig paymentConfig, final InvoiceInternalApi invoiceApi, final TagUserApi tagApi, final PaymentDao paymentDao,
+                                          final InvoicePaymentControlDao invoicePaymentControlDao,
+                                          @Named(PaymentModule.RETRYABLE_NAMED) final RetryServiceScheduler retryServiceScheduler,
                                           final InternalCallContextFactory internalCallContextFactory, final Clock clock) {
         this.paymentConfig = paymentConfig;
         this.invoiceApi = invoiceApi;
         this.tagApi = tagApi;
         this.paymentDao = paymentDao;
+        this.controlDao = invoicePaymentControlDao;
+        this.retryServiceScheduler = retryServiceScheduler;
         this.internalCallContextFactory = internalCallContextFactory;
         this.clock = clock;
-    }
-
-    private boolean scheduleRetryForAccountWith_AUTO_PAY_OFF(final PaymentControlContext paymentControlContext) {
-        if (!isAccountAutoPayOff(paymentControlContext.getAccount().getId(), paymentControlContext)) {
-            return false;
-        }
-    // STEPH
-        return true;
-
-    }
-
-    private boolean isAccountAutoPayOff(final UUID accountId, final CallContext callContext) {
-        final List<Tag> accountTags = tagApi.getTagsForAccount(accountId, false, callContext);
-        return ControlTagType.isAutoPayOff(Collections2.transform(accountTags, new Function<Tag, UUID>() {
-            @Override
-            public UUID apply(final Tag tag) {
-                return tag.getTagDefinitionId();
-            }
-        }));
     }
 
 
@@ -114,6 +110,10 @@ public final class InvoicePaymentControlPluginApi implements PaymentControlPlugi
         final TransactionType transactionType = paymentControlContext.getTransactionType();
         Preconditions.checkArgument(transactionType == TransactionType.PURCHASE ||
                                    transactionType == TransactionType.REFUND);
+
+        if (insert_AUTO_PAY_OFF_ifRequired(paymentControlContext)) {
+            return new DefaultPriorPaymentControlResult(true);
+        }
 
         final InternalCallContext internalContext = internalCallContextFactory.createInternalCallContext(paymentControlContext.getAccount().getId(), paymentControlContext);
         if (transactionType == TransactionType.PURCHASE) {
@@ -153,6 +153,14 @@ public final class InvoicePaymentControlPluginApi implements PaymentControlPlugi
             default:
                 return new DefaultFailureCallResult(null);
         }
+    }
+
+    public void process_AUTO_PAY_OFF_removal(final Account account, final InternalCallContext internalCallContext) {
+        final List<PluginAutoPayOffModelDao> entries = controlDao.getAutoPayOffEntry(account.getId());
+        for (PluginAutoPayOffModelDao cur : entries) {
+            retryServiceScheduler.scheduleRetry(cur.getPaymentId(), cur.getTransactionExternalKey(), PLUGIN_NAME, clock.getUTCNow());
+        }
+        controlDao.removeAutoPayOffEntry(account.getId());
     }
 
     private PriorPaymentControlResult getPluginPurchaseResult(final PaymentControlContext paymentControlPluginContext, final InternalCallContext internalContext) throws PaymentControlApiException {
@@ -345,4 +353,23 @@ public final class InvoicePaymentControlPluginApi implements PaymentControlPlugi
         }
     }
 
+    private boolean insert_AUTO_PAY_OFF_ifRequired(final PaymentControlContext paymentControlContext) {
+        if (!isAccountAutoPayOff(paymentControlContext.getAccount().getId(), paymentControlContext)) {
+            return false;
+        }
+        final PluginAutoPayOffModelDao data = new PluginAutoPayOffModelDao(paymentControlContext.getPaymentExternalKey(), paymentControlContext.getTransactionExternalKey(), paymentControlContext.getAccount().getId(), PLUGIN_NAME,
+                                                                           paymentControlContext.getPaymentId(), paymentControlContext.getPaymentMethodId(), paymentControlContext.getAmount(), paymentControlContext.getCurrency(), CREATED_BY, clock.getUTCNow());
+        controlDao.insertAutoPayOff(data);
+        return true;
+    }
+
+    private boolean isAccountAutoPayOff(final UUID accountId, final CallContext callContext) {
+        final List<Tag> accountTags = tagApi.getTagsForAccount(accountId, false, callContext);
+        return ControlTagType.isAutoPayOff(Collections2.transform(accountTags, new Function<Tag, UUID>() {
+            @Override
+            public UUID apply(final Tag tag) {
+                return tag.getTagDefinitionId();
+            }
+        }));
+    }
 }

payment/src/main/java/org/killbill/billing/payment/control/PaymentTagHandler.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-package org.killbill.billing.payment.bus;
+package org.killbill.billing.payment.control;
 
 import java.util.UUID;
 
@@ -24,8 +24,10 @@ import org.killbill.billing.account.api.AccountApiException;
 import org.killbill.billing.account.api.AccountInternalApi;
 import org.killbill.billing.callcontext.InternalCallContext;
 import org.killbill.billing.events.ControlTagDeletionInternalEvent;
+import org.killbill.billing.osgi.api.OSGIServiceRegistration;
 import org.killbill.billing.payment.api.PaymentApiException;
 import org.killbill.billing.payment.core.DirectPaymentProcessor;
+import org.killbill.billing.retry.plugin.api.PaymentControlPluginApi;
 import org.killbill.billing.util.callcontext.CallOrigin;
 import org.killbill.billing.util.callcontext.InternalCallContextFactory;
 import org.killbill.billing.util.callcontext.UserType;
@@ -45,15 +47,20 @@ public class PaymentTagHandler {
     private final AccountInternalApi accountApi;
     private final DirectPaymentProcessor paymentProcessor;
     private final InternalCallContextFactory internalCallContextFactory;
+    private final OSGIServiceRegistration<PaymentControlPluginApi> paymentControlPluginRegistry;
+    private final PaymentControlPluginApi invoicePaymentControlPlugin;
 
     @Inject
     public PaymentTagHandler(final Clock clock,
                              final AccountInternalApi accountApi,
                              final DirectPaymentProcessor paymentProcessor,
+                             final OSGIServiceRegistration<PaymentControlPluginApi> paymentControlPluginRegistry,
                              final InternalCallContextFactory internalCallContextFactory) {
         this.clock = clock;
         this.accountApi = accountApi;
         this.paymentProcessor = paymentProcessor;
+        this.paymentControlPluginRegistry = paymentControlPluginRegistry;
+        this.invoicePaymentControlPlugin = paymentControlPluginRegistry.getServiceForName(InvoicePaymentControlPluginApi.PLUGIN_NAME);
         this.internalCallContextFactory = internalCallContextFactory;
     }
 
@@ -71,11 +78,9 @@ public class PaymentTagHandler {
             final InternalCallContext internalCallContext = internalCallContextFactory.createInternalCallContext(tenantRecordId, accountRecordId,
                                                                                                                  "PaymentRequestProcessor", CallOrigin.INTERNAL, UserType.SYSTEM, userToken);
             final Account account = accountApi.getAccountById(accountId, internalCallContext);
-            paymentProcessor.process_AUTO_PAY_OFF_removal(account, internalCallContext);
+            ((InvoicePaymentControlPluginApi) invoicePaymentControlPlugin).process_AUTO_PAY_OFF_removal(account, internalCallContext);
 
         } catch (AccountApiException e) {
-            log.warn(String.format("Failed to process process  removal AUTO_PAY_OFF for account %s", accountId), e);
-        } catch (PaymentApiException e) {
             log.warn(String.format("Failed to process process  removal AUTO_PAY_OFF for account %s", accountId), e);
         }
     }

payment/src/main/java/org/killbill/billing/payment/control/dao/InvoicePaymentControlDao.java
@@ -24,9 +24,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.inject.Inject;
+
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.killbill.billing.catalog.api.Currency;
+import org.killbill.billing.payment.dao.PluginPropertyModelDao;
 import org.skife.jdbi.v2.Handle;
 import org.skife.jdbi.v2.IDBI;
 import org.skife.jdbi.v2.PreparedBatch;
@@ -41,6 +44,7 @@ public class InvoicePaymentControlDao {
 
     private final IDBI dbi;
 
+    @Inject
     public InvoicePaymentControlDao(final IDBI dbi) {
         this.dbi = dbi;
     }
@@ -89,51 +93,12 @@ public class InvoicePaymentControlDao {
         });
     }
 
-    public void insertPluginProperties(final List<PluginPropertyModelDao> dataEntries) {
-        dbi.inTransaction(new TransactionCallback<Void>() {
+    public void removeAutoPayOffEntry(final UUID accountId) {
+        dbi.withHandle(new HandleCallback<Void>() {
             @Override
-            public Void inTransaction(final Handle handle, final TransactionStatus status) throws Exception {
-
-                final PreparedBatch batch = handle.prepareBatch("insert into _invoice_payment_control_plugin_properties " +
-                                                                "(payment_external_key, transaction_external_key, account_id, plugin_name, prop_key, prop_value, created_by, created_date) values " +
-                                                                "(?,?,?,?,?,?,?,?)");
-                for (PluginPropertyModelDao data : dataEntries) {
-                    final PreparedBatchPart entry = batch.add();
-                    entry.bind(0, data.getPaymentExternalKey())
-                         .bind(1, data.getTransactionExternalKey())
-                         .bind(2, data.getAccountId().toString())
-                         .bind(3, data.getPluginName())
-                         .bind(4, data.getPropKey())
-                         .bind(5, data.getPropValue())
-                         .bind(6, data.getCreatedBy())
-                         .bind(7, data.getCreatedDate());
-                }
-                batch.execute();
+            public Void withHandle(final Handle handle) throws Exception {
+                handle.execute("delete from _invoice_payment_control_plugin_auto_pay_off where account_id = ?", accountId.toString());
                 return null;
-            }
-        });
-    }
-
-    public List<PluginPropertyModelDao> getPluginProperties(final String transactionExternalkey) {
-
-        return dbi.withHandle(new HandleCallback<List<PluginPropertyModelDao>>() {
-            @Override
-            public List<PluginPropertyModelDao> withHandle(final Handle handle) throws Exception {
-                final List<Map<String, Object>> queryResult = handle.select("select * from _invoice_payment_control_plugin_properties where transaction_external_key = ?", transactionExternalkey);
-                final List<PluginPropertyModelDao> result = new ArrayList<PluginPropertyModelDao>(queryResult.size());
-                for (final Map<String, Object> row : queryResult) {
-                    final PluginPropertyModelDao entry = new PluginPropertyModelDao((Long) row.get("record_id"),
-                                                                                    (String) row.get("payment_external_key"),
-                                                                                    (String) row.get("transaction_external_key"),
-                                                                                    UUID.fromString((String) row.get("account_id")),
-                                                                                    (String) row.get("plugin_name"),
-                                                                                    (String) row.get("prop_key"),
-                                                                                    (String) row.get("prop_value"),
-                                                                                    (String) row.get("created_by"),
-                                                                                    getDateTime(row.get("created_date")));
-                    result.add(entry);
-                }
-                return result;
             }
         });
     }

payment/src/main/java/org/killbill/billing/payment/core/DirectPaymentProcessor.java
@@ -261,10 +261,6 @@ public class DirectPaymentProcessor extends ProcessorBase {
         return toDirectPayment(paymentModelDao, transactionsForDirectPayment, pluginInfo);
     }
 
-    public void process_AUTO_PAY_OFF_removal(final Account account, final InternalCallContext context) throws PaymentApiException {
-        // STEPH TODO implement AUTO_PAY_OFF
-    }
-
     public Pagination<DirectPayment> getPayments(final Long offset, final Long limit, final Iterable<PluginProperty> properties,
                                                  final TenantContext tenantContext, final InternalTenantContext internalTenantContext) {
         return getEntityPaginationFromPlugins(getAvailablePlugins(),

payment/src/main/java/org/killbill/billing/payment/core/PluginControlledPaymentProcessor.java
@@ -17,6 +17,7 @@
 package org.killbill.billing.payment.core;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
@@ -41,6 +42,7 @@ import org.killbill.billing.payment.dao.DirectPaymentModelDao;
 import org.killbill.billing.payment.dao.DirectPaymentTransactionModelDao;
 import org.killbill.billing.payment.dao.PaymentAttemptModelDao;
 import org.killbill.billing.payment.dao.PaymentDao;
+import org.killbill.billing.payment.dao.PluginPropertyModelDao;
 import org.killbill.billing.payment.plugin.api.PaymentPluginApi;
 import org.killbill.billing.tag.TagInternalApi;
 import org.killbill.billing.util.callcontext.CallContext;
@@ -48,6 +50,9 @@ import org.killbill.billing.util.dao.NonEntityDao;
 import org.killbill.bus.api.PersistentBus;
 import org.killbill.commons.locker.GlobalLocker;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.inject.name.Named;
 
 import static org.killbill.billing.payment.glue.PaymentModule.PLUGIN_EXECUTOR_NAMED;
@@ -170,12 +175,22 @@ public class PluginControlledPaymentProcessor extends ProcessorBase {
                                                          callContext, internalCallContext);
     }
 
-    public void retryPaymentTransaction(final String transactionExternalKey, final Iterable<PluginProperty> properties, final InternalCallContext internalCallContext) {
+    public void retryPaymentTransaction(final String transactionExternalKey, final InternalCallContext internalCallContext) {
         try {
 
             final PaymentAttemptModelDao attempt = paymentDao.getPaymentAttemptByExternalKey(transactionExternalKey, internalCallContext);
             final DirectPaymentTransactionModelDao transaction = paymentDao.getDirectPaymentTransactionByExternalKey(transactionExternalKey, internalCallContext);
             final DirectPaymentModelDao payment = paymentDao.getDirectPayment(transaction.getDirectPaymentId(), internalCallContext);
+
+            final List<PluginPropertyModelDao> properties = paymentDao.getProperties(transactionExternalKey, internalCallContext);
+
+            final List<PluginProperty> pluginProperties = ImmutableList.copyOf(Iterables.transform(properties, new Function<PluginPropertyModelDao, PluginProperty>() {
+                @Nullable
+                @Override
+                public PluginProperty apply(final PluginPropertyModelDao input) {
+                    return new PluginProperty(input.getPropKey(), input.getPropValue(), false);
+                }
+            }));
 
             final boolean isExternalPayment = false;
             final Account account = accountInternalApi.getAccountById(payment.getAccountId(), internalCallContext);
@@ -194,7 +209,7 @@ public class PluginControlledPaymentProcessor extends ProcessorBase {
                                                       transaction.getAmount(),
                                                       transaction.getCurrency(),
                                                       isExternalPayment,
-                                                      properties,
+                                                      pluginProperties,
                                                       null,
                                                       callContext,
                                                       internalCallContext);

commit b523e70bd907db9a075a2a02de009aa9c4383cdf

entity: fix bug in EntityBase#equals
This was introduced by 7aaca3914cc89bfdc1d652fe04095c4f6e7bfca5.

Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

api/src/main/java/org/killbill/billing/entity/EntityBase.java
@@ -90,10 +90,10 @@ public abstract class EntityBase implements Entity {
         if (id != null ? !id.equals(that.id) : that.id != null) {
             return false;
         }
-        if (createdDate != null ? createdDate.compareTo(that.createdDate) == 0 : that.createdDate != null) {
+        if (createdDate != null ? createdDate.compareTo(that.createdDate) != 0 : that.createdDate != null) {
             return false;
         }
-        if (updatedDate != null ? updatedDate.compareTo(that.updatedDate) == 0 : that.updatedDate != null) {
+        if (updatedDate != null ? updatedDate.compareTo(that.updatedDate) != 0 : that.updatedDate != null) {
             return false;
         }
 